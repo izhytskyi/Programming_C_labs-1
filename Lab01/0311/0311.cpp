5e20f8fbd33df7733b0bf060e1256d33f6fa5b83
aruFesT <elenacom929@gmail.com>
2022-02-19 23:08:52 +0200

    010203

--- Lab01/0311/0311.cpp
+++ Lab01/0311/0311.cpp
@@ -5,16 +5,19 @@ using namespace std;
 
 int main()
 {
-    double R2, R1, S,pi;
+    double R2, R1, S, pi;
     cout << "Enter R1" << endl;
     cin >> R1;
     cout << "Enter R2" << endl;
     cin >> R2;
     pi = 3.14;
-    R1 < R2;
-    S = pi * (pow (R2,2) - pow (R1,2));
+    S = pi * (pow(R2, 2) - pow(R1, 2));
+    if (R1 < R2)
     cout << S << endl;
-
-
+    else if (R1 == R2)
+    cout << "vonu rivni" << endl;
+    else
+    cout << "R1 bilshe R2" << endl;
+  
 
 }
